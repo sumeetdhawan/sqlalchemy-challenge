cce2f032cc241840e5fcf75e8780969aad01dcbe
sumeetdhawan <59894796+sumeetdhawan@users.noreply.github.com>
2020-04-12 13:18:45 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+from flask import Flask, jsonify
+import datetime as dt
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False}, echo=True)
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/2016-11-15<br/>"  
+        f"/api/v1.0/2016-11-15/2016-12-15<br/>"  
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+    # Design a query to retrieve the last 12 months of precipitation data and plot the results
+    max_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+
+    # Get the first element of the tuple
+    max_date = max_date[0]
+
+    # Calculate the date 1 year ago from today
+    year_ago = dt.datetime.strptime(max_date, "%Y-%m-%d") - dt.timedelta(days=366)
+    
+    # Perform a query to retrieve the data and precipitation scores
+    results_precipitation = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
+
+    # Convert list of tuples into normal list
+    precipitation_dict = dict(results_precipitation)
+
+    return jsonify(precipitation_dict)
+
+@app.route("/api/v1.0/stations")
+def stations(): 
+
+    # Query stations
+    results_stations =  session.query(Measurement.station).group_by(Measurement.station).all()
+
+    # Convert list of tuples into normal list
+    stations_list = list(np.ravel(results_stations))
+
+    return jsonify(stations_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs(): 
+
+    # Design a query to retrieve the last 12 months of precipitation data and plot the results
+    max_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+
+    # Get the first element of the tuple
+    max_date = max_date[0]
+
+    # Calculate the date 1 year ago from today
+    year_ago = dt.datetime.strptime(max_date, "%Y-%m-%d") - dt.timedelta(days=366)
+    # Query tobs
+    results_tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= year_ago).all()
+
+    # Convert list of tuples into normal list
+    tobs_list = list(results_tobs)
+
+    return jsonify(tobs_list)
+
+
+
+@app.route("/api/v1.0/<start>")
+def start(start=None):
+
+    from_start = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).group_by(Measurement.date).all()
+    from_start_list=list(from_start)
+    return jsonify(from_start_list)
+
+    
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start=None, end=None):
+     
+    between_dates = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()
+    between_dates_list=list(between_dates)
+    return jsonify(between_dates_list)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
